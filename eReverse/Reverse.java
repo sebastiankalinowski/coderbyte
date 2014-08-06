25b5ccccf644f08ad7a40f52cf3db1e42a985bc6
Sebastian Kalinowski <sebastian.kalinowski@gmail.com>
2014-08-05 17:24:54 -0700

    Reverse java updated

--- eReverse/Reverse.java
+++ eReverse/Reverse.java
@@ -1,5 +1,7 @@
+public class Reverse {
 public static void main(String[] args) {
 	String str="Hello Java!";
 	String reverse = new StringBuffer(str).reverse().toString();
 	System.out.println(reverse);
+	}
 }
